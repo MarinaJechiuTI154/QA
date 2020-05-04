16c4143969534dd631cfc83cb0c2c43bb7441a64
Marina Plugaru <marina.plugaru@amdaris.com>
2020-05-04 11:31:12 +0300

    unit tests

---
+++ UnitTest/Business.Tests/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Business.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void First()
+        {
+            Assert.That(1, Is.EqualTo(1));
+        }
+    }
+}
